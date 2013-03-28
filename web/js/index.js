a4e3fef05536cf7945c8be3624b4d33ccfc59b5e
xiaobai <huibo5263285@163.com>
2013-03-28 18:04:28 +0800

    ok

---
+++ web/js/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13-3-28
+ * Time: 上午9:59
+ */
---
+++ web/js/sub.js
@@ -0,0 +1,8 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13-3-28
+ * Time: 上午9:59
+ */
+
+
